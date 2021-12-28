21e105c70cc2af6a6c8754b907460c47a8afb4f8
Uiara <uiarabianka87@gmail.com>
2021-12-28 14:07:48 -0300

    exercicio 153

---
+++ 1153.py
@@ -0,0 +1,15 @@
+#Ler um valor N. Calcular e escrever seu respectivo fatorial. 
+# Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+
+n = int(input())
+n = int(input())
+y = 1
+
+saida = 1
+
+while n > y :
+    saida = saida * y
+    y += 1
+saida = saida * n
+print(saida)
+
